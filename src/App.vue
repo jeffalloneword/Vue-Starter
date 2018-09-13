6cbf97e4a349a2c73e387964da96f331c6372ed4
Jeff Hyatt <jeffalloneword@users.noreply.github.com>
2018-09-13 11:19:37 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <InputTest/>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld"
+import InputTest from "./components/InputTest"
+
+export default {
+  name: "App",
+  components: {
+    InputTest,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InputTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ header }}</h1>
+    <input type='text' v-model='header' />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputTest",
+  data() {
+    return {
+      header: "Here's some Vue data!"
+    };
+  }
+};
+</script>
